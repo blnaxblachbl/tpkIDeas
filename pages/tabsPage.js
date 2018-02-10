f295aba23f1a9d9d51d1ec8c706865738c94d77a
Titeverstov <blnaxblachbl@gmail.com>
2018-02-10 17:51:46 +0900

    logo

--- pages/tabsPage.js
+++ pages/tabsPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Tabs } from "../routes/tabNavigator";
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 
 class TabsPage extends Component {
     static navigationOptions = {
-        title: ' ',
+        title: (
+            <Image source={require("../assets/Logo.png")}/>
+        ),
+        headerTitleStyle: {
+            alignSelf: "center"
+        },
         headerLeft: (
             <Icon name="search" style={{marginLeft: 15}} size={30} color={"black"}/>
         ),
