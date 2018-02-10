ad0e90c569c75ec2064cdafd321daac827e157c8
Titeverstov <blnaxblachbl@gmail.com>
2018-02-11 02:52:43 +0900

    login and registr

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -43,46 +43,40 @@ class LoginPage extends Component {
     render() {
         const { navigate } = this.props.navigation
         return (
-            <View style={{
-                flex: 1,
-                backgroundColor: '#fff',
-            }}>
-                <RkAvoidKeyboard style={styles.container}>
-                    <View style={{ width: "80%" }}>
-                        <RkTextInput
-                            rkType='rounded'
-                            onChangeText={(text) => { this.setState({ login: text }) }}
-                            placeholder='Почта'
-                            inputStyle={{
-                                backgroundColor: 'transparent',
-                                color: 'black',
-                            }}
-                            caretHidden={true}
-                            autoCorrect={false}
-                            style={{ height: 50 }}
-                        />
-                        <RkTextInput
-                            rkType='rounded'
-                            onChangeText={(text) => { this.setState({ password: text }) }}
-                            placeholder='Пароль'
-                            inputStyle={{
-                                backgroundColor: 'transparent',
-                                color: 'black',
-                            }}
-                            caretHidden={true}
-                            autoCorrect={false}
-                            secureTextEntry={true}
-                            style={{ height: 50 }}
-                        />
-                        <RkButton rkType='outline' style={{ height: 50, marginTop: 10, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { this.login() }}>
-                            Войти
+            <View style={styles.container}>
+                <View style={{ width: "80%" }}>
+                    <RkTextInput
+                        rkType='rounded'
+                        onChangeText={(text) => { this.setState({ login: text }) }}
+                        placeholder='Почта'
+                        inputStyle={{
+                            backgroundColor: 'transparent',
+                            color: 'black',
+                        }}
+                        caretHidden={true}
+                        autoCorrect={false}
+                        style={{ height: 50 }}
+                    />
+                    <RkTextInput
+                        rkType='rounded'
+                        onChangeText={(text) => { this.setState({ password: text }) }}
+                        placeholder='Пароль'
+                        inputStyle={{
+                            backgroundColor: 'transparent',
+                            color: 'black',
+                        }}
+                        caretHidden={true}
+                        autoCorrect={false}
+                        secureTextEntry={true}
+                        style={{ height: 50 }}
+                    />
+                    <RkButton rkType='outline' style={{ height: 50, marginTop: 10, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { this.login() }}>
+                        Войти
                     </RkButton>
-                        <RkButton rkType='outline' style={{ height: 50, marginTop: 20, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { navigate('Registration') }}>
-                            Зарегистрироваться
+                    <RkButton rkType='outline' style={{ height: 50, marginTop: 20, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { navigate('Registration') }}>
+                        Зарегистрироваться
                     </RkButton>
-                    </View>
-                    <View style={{ height: 30 }} />
-                </RkAvoidKeyboard>
+                </View>
             </View>
         );
     }
@@ -93,7 +87,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'flex-end',
+        justifyContent: 'flex-start',
+        paddingTop: 50
     },
 });
 
--- pages/registrationPage.js
+++ pages/registrationPage.js
@@ -111,7 +111,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'flex-end',
+        justifyContent: 'flex-start',
     },
 });
 
