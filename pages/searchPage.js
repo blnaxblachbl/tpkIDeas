c81d88e3df16419ffe21a527526492eca5eeca69
Titeverstov <blnaxblachbl@gmail.com>
2018-02-10 22:58:14 +0900

    фыв

--- pages/searchPage.js
+++ pages/searchPage.js
@@ -45,7 +45,7 @@ class SearchPage extends Component {
                     name: 'Надежда',
                     surname: 'Максимова',
                     spesialization: 'Компас',
-                    img: 'http://img1.reactor.cc/pics/post/full/%D0%BF%D0%B5%D1%81%D0%BE%D1%87%D0%BD%D0%B8%D1%86%D0%B0-%D1%83%D0%BF%D0%BE%D1%80%D0%BE%D1%82%D0%B0%D1%8F-%D0%BB%D0%B8%D1%81%D0%B0-%D0%9B%D0%B8%D1%81%D0%B0-%D0%A7%D1%83%D1%87%D0%B5%D0%BB%D0%BE-%D0%BB%D0%B8%D1%81%D1%8B-971285.jpeg'
+                    img: 'https://pp.userapi.com/c316425/v316425876/569d/5Lxr969sfJo.jpg'
                 }
             ],
             search: ""
