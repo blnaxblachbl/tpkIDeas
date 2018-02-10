f1e1124ad8f867ce40a84ee41fbe9039cbd301f2
bairock <bairock@live.ru>
2018-02-10 22:43:51 +0900

    21412
    4124124

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -43,37 +43,47 @@ class LoginPage extends Component {
     render() {
         const { navigate } = this.props.navigation
         return (
-            <RkAvoidKeyboard style={styles.container}>
-                <View style={{ width: "80%", paddingBottom: 30 }}>
-                    <RkTextInput
-                        rkType='rounded'
-                        onChangeText={(text) => { this.setState({ login: text }) }}
-                        placeholder='Почта'
-                        inputStyle={{
-                            backgroundColor: 'transparent',
-                            color: 'black',
-                        }}
-                        style={{ height: 50 }}
-                    />
-                    <RkTextInput
-                        rkType='rounded'
-                        onChangeText={(text) => { this.setState({ password: text }) }}
-                        placeholder='Пароль'
-                        inputStyle={{
-                            backgroundColor: 'transparent',
-                            color: 'black',
-                        }}
-                        secureTextEntry={true}
-                        style={{ height: 50 }}
-                    />
-                    <RkButton rkType='outline' style={{ height: 50, marginTop: 10, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { this.login() }}>
-                        Войти
+            <View style={{
+                flex: 1,
+                backgroundColor: '#fff',
+            }}>
+                <RkAvoidKeyboard style={styles.container}>
+                    <View style={{ width: "80%" }}>
+                        <RkTextInput
+                            rkType='rounded'
+                            onChangeText={(text) => { this.setState({ login: text }) }}
+                            placeholder='Почта'
+                            inputStyle={{
+                                backgroundColor: 'transparent',
+                                color: 'black',
+                            }}
+                            caretHidden={true}
+                            autoCorrect={false}
+                            style={{ height: 50 }}
+                        />
+                        <RkTextInput
+                            rkType='rounded'
+                            onChangeText={(text) => { this.setState({ password: text }) }}
+                            placeholder='Пароль'
+                            inputStyle={{
+                                backgroundColor: 'transparent',
+                                color: 'black',
+                            }}
+                            caretHidden={true}
+                            autoCorrect={false}
+                            secureTextEntry={true}
+                            style={{ height: 50 }}
+                        />
+                        <RkButton rkType='outline' style={{ height: 50, marginTop: 10, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { this.login() }}>
+                            Войти
                     </RkButton>
-                    <RkButton rkType='outline' style={{ height: 50, marginTop: 20, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { navigate('Registration') }}>
-                        Зарегистрироваться
+                        <RkButton rkType='outline' style={{ height: 50, marginTop: 20, backgroundColor: 'transparent', width: '100%', borderColor: 'black', borderRadius: 30 }} contentStyle={{ color: 'black' }} onPress={() => { navigate('Registration') }}>
+                            Зарегистрироваться
                     </RkButton>
-                </View>
-            </RkAvoidKeyboard>
+                    </View>
+                    <View style={{ height: 30 }} />
+                </RkAvoidKeyboard>
+            </View>
         );
     }
 }
